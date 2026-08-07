96d69829ef4e54bfa447ae5dbd724c84a4e92fd5
Prashant <prashantrai325@gmail.com>
2026-08-07 10:40:02 +0530

    Configure and create role migration by alembic

---
+++ app/models/__init__.py
@@ -0,0 +1,1 @@
+from app.models.role import Role
--- app/models/role.py
+++ app/models/role.py
@@ -5,5 +5,5 @@ from app.db.base import Base
 class Role(Base):
     __tablename__="roles"
 
-    id: Mapped[int] = mapped_column(primary_key=True, index=True)             # "Mapped" type hints
+    id: Mapped[int] = mapped_column(primary_key=True)             # "Mapped" type hints
     name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False) # that name is a string column managed by the ORM.
